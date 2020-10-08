a8fc71ce0eb5e10ce6023b88ada93837e99040c7
Damian Zielonka <damianogreen@o2.pl>
2020-10-08 19:47:09 +0200

    Add modified: index.html

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 'use strict';
 
 function titleClickHandler(event){
+	event.preventDefault();
   const clickedElement = this;
   console.log('Link was clicked!');
+
 
   /* [DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
@@ -14,6 +16,7 @@ for(let activeLink of activeLinks){
   /* [IN PROGRESS] add class 'active' to the clicked link */
   console.log('clickedElement:', clickedElement);
   	activeLink.classList.add('active');
+  
 
   /* [DONE] remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.titles a');
@@ -23,8 +26,14 @@ for(let activeArticle of activeArticles){
 }
   
   /* find the correct article using the selector (value of 'href' attribute) */
+  const articleSelector = clickedElement.getAttribute('#article-1');
+	console.log('Link was clicked!');
+const targetArticle = document.querySelector('#article-1');
+	console.log('Link was clicked!');
 
   /* add class 'active' to the correct article */
+console.log('clickedElement:', clickedElement);
+  	activeArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
